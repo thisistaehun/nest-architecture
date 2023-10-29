b22cc2a7b1adef81ab037338254d25271e787032
thisistaehun <taehun@tesser.co.kr>
2023-10-29 16:00:26 +0900

    fix: enum register

--- src/modules/api/point/type/point.transaction.type.ts
+++ src/modules/api/point/type/point.transaction.type.ts
@@ -1,4 +1,11 @@
+import { registerEnumType } from '@nestjs/graphql';
+
 export enum PointTransactionType {
   CHARGE = 'CHARGE',
   USE = 'USE',
 }
+
+registerEnumType(PointTransactionType, {
+  name: 'PointTransactionType',
+  description: '포인트 트랜잭션 타입',
+});
