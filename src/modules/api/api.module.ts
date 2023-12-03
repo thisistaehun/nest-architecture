97ef7ce5639977109f6d714de3c352279ffd0ad8
taehunkim <117618060+thisistaehun@users.noreply.github.com>
2023-12-04 00:05:56 +0900

    rename: folder rename

--- src/modules/api/api.module.ts
+++ src/modules/api/api.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+import { SearchKeywordModule } from './competition-analysis/search-keyword.module';
 import { PaymentModule } from './payment/payment.module';
 import { PointModule } from './point/point.module';
 import { UserRankPolicyModule } from './policy/user-rank.policy.module';
 import { PostAnalysisModule } from './post-analysis/post-analysis.module';
 import { ProductModule } from './product/product.module';
-import { SearchKeywordModule } from './search-keyword/search-keyword.module';
 import { UserModule } from './user/user.module';
 
 @Module({
