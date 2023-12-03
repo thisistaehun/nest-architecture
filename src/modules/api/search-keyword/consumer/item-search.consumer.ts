3e4eb042fea38fedea031830c7ace4857853f095
taehunkim <117618060+thisistaehun@users.noreply.github.com>
2023-12-04 00:02:42 +0900

    fix: view search 로직 ㅛㅜ정

--- src/modules/api/search-keyword/consumer/item-search.consumer.ts
+++ src/modules/api/search-keyword/consumer/item-search.consumer.ts
@@ -1,7 +1,6 @@
 import { Process, Processor } from '@nestjs/bull';
 import { Inject } from '@nestjs/common';
 import { Job } from 'bull';
-import * as puppeteer from 'puppeteer';
 import { DgLoggerImpl } from 'src/modules/infrastructure/logger/logger.implement';
 import { DG_LOGGER } from 'src/symbols';
 import { ItemSearchTransaction } from '../cqrs/command/item-search.transaction';
@@ -13,47 +12,15 @@ export class ItemSearchConsumer {
   constructor(
     @Inject(DG_LOGGER)
     private readonly logger: DgLoggerImpl,
-    private readonly searchCommandRepository: ItemSearchTransaction,
+    private readonly itemSearchTransaction: ItemSearchTransaction,
   ) {}
 
   @Process('item-search')
   async execute(job: Job) {
     this.logger.log(`start consume item-search job`);
-    const crawledData = await this.crawlType(job.data.name);
+
     const keywordEntity = new SearchKeyword(job.data);
-    keywordEntity.type = crawledData.type;
-    await this.searchCommandRepository.execute(
-      keywordEntity,
-      job.data.userCode,
-    );
-  }
-
-  private async crawlType(keyword: string) {
-    const browser = await puppeteer.launch({
-      headless: 'new',
-    });
-    const page = await browser.newPage();
-    await page.goto(
-      `https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=${keyword}`,
-    );
-
-    const data = await page.evaluate(() => {
-      const elements = Array.from(
-        document.querySelectorAll('.fds-comps-header-headline'),
-      );
-      return elements.map((element) => element.textContent.trim());
-    });
-    this.logger.log(`sucessfully crawl search type`);
-    if (data.length === 0) {
-      return {
-        type: CrawlSearchType.VIEW,
-        data,
-      };
-    }
-
-    return {
-      type: CrawlSearchType.AIR_SEARCH,
-      data,
-    };
+    keywordEntity.type = CrawlSearchType.VIEW;
+    await this.itemSearchTransaction.execute(keywordEntity, job.data.userCode);
   }
 }
